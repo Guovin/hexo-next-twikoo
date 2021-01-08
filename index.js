d98becde021d29a65e87b557eaf514034e8064e3
iMaeGoo <mail1st@qq.com>
2021-01-08 16:18:13 +0800

    init hexo-next-twikoo

--- index.js
+++ index.js
@@ -5,73 +5,31 @@
 const Util = require('@next-theme/utils');
 const utils = new Util(hexo, __dirname);
 
-function capitalize(input) {
-  return input.toString().charAt(0).toUpperCase() + input.toString().substr(1);
-}
-
-function iconText(icon, key, defaultValue) {
-  if (!defaultValue) {
-    defaultValue = capitalize(key);
-  }
-  return `
-    <span class="post-meta-item-icon">
-      <i class="${icon}"></i>
-    </span>
-    {%- set post_meta_comment = __('post.comments.${key}') %}
-    {%- if post_meta_comment == 'post.comments.${key}' %}
-      {%- set post_meta_comment = '${defaultValue}' %}
-    {%- endif %}
-    <span class="post-meta-item-text">{{ post_meta_comment + __('symbol.colon') }}</span>
-  `;
-}
-
-function warning(...args) {
-  hexo.log.warn(`Since ${args[0]} is turned on, the ${args[1]} is disabled to avoid potential hazards.`);
-};
-
 // Add comment
 hexo.extend.filter.register('theme_inject', injects => {
 
-  const config = utils.defaultConfigFile('valine', 'default.yaml');
-  if (!config.enable || !config.appId || !config.appKey) return;
+  const config = utils.defaultConfigFile('twikoo', 'default.yaml');
+  if (!config.enable || !config.envId) return;
 
-  injects.comment.raw('valine', '<div class="comments" id="valine-comments"></div>', {}, { cache: true });
+  injects.comment.raw('twikoo', '<div class="comments"><div id="twikoo-comments"></div></div>', {}, { cache: true });
 
-  injects.bodyEnd.raw('valine', utils.getFileContent('valine.njk'));
+  injects.bodyEnd.raw('twikoo', utils.getFileContent('twikoo.njk'));
 
 });
 
 // Add post_meta
 hexo.extend.filter.register('theme_inject', injects => {
 
-  const config = utils.defaultConfigFile('valine', 'default.yaml');
-  if (!config.enable || !config.appId || !config.appKey) return;
-
-  injects.postMeta.raw('valine', `
-  {% if post.comments and (is_post() or theme.valine.comment_count) %}
-  <span class="post-meta-item">
-    ${iconText('far fa-comment', 'valine')}
-    <a title="valine" href="{{ url_for(post.path) }}#valine-comments" itemprop="discussionUrl">
-      <span class="post-comments-count valine-comment-count" data-xid="{{ url_for(post.path) }}" itemprop="commentCount"></span>
-    </a>
-  </span>
-  {% endif %}
-  `, {}, {});
-
+  const config = utils.defaultConfigFile('twikoo', 'default.yaml');
+  if (!config.enable || !config.envId) return;
   if (config.visitor) {
-    if (hexo.theme.config.leancloud_visitors && hexo.theme.config.leancloud_visitors.enable) {
-      warning('valine.visitor', 'leancloud_visitors');
-      hexo.theme.config.leancloud_visitors.enable = false;
-      return;
-    }
-
-    injects.postMeta.raw('valine', `
-    <span id="{{ url_for(post.path) }}" class="post-meta-item leancloud_visitors" data-flag-title="{{ post.title }}" title="{{ __('post.views') }}">
+    injects.postMeta.raw('twikoo', `
+    <span id="{{ url_for(post.path) }}" class="post-meta-item twikoo_visitors" data-flag-title="{{ post.title }}" title="{{ __('post.views') }}">
       <span class="post-meta-item-icon">
         <i class="far fa-eye"></i>
       </span>
       <span class="post-meta-item-text">{{ __('post.views') + __('symbol.colon') }}</span>
-      <span class="leancloud-visitors-count"></span>
+      <span id="twikoo_visitors"></span>
     </span>
   `, {}, {});
   }
